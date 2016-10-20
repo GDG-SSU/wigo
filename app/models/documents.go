87ce84378dc9307e8d2e239aced72cf0e309ce2a
Namyun Kim <namyun.me@gmail.com>
2016-10-20 17:18:29 +0900

    `documents` 테이블의 컬럼 생성 순서 설정
    `Document` 모델을 변경하여 `documents` 테이블의 컬럼 순서가 명시적
    으로 정렬됩니다.

--- app/models/documents.go
+++ app/models/documents.go
@@ -1,12 +1,15 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import "time"
 
 /**
  * Document - A database model representing Wiki document
  */
 type Document struct {
-	gorm.Model
+	ID			uint
 	Title   string `gorm:"size:255"sql:"not null;"`
 	Content string `sql:"type:text;not null;"`
+	CreatedAt	time.Time
+	UpdatedAt	time.Time
+	DeletedAt	*time.Time
 }
