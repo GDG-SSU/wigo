d0356bd5694358dcd794d8d2906ade68e267c9dc
Namyun Kim <namyun.me@gmail.com>
2016-10-21 20:47:01 +0900

    EditorConfig 설정
    코딩 스타일을 정의하는 EditorConfig 설정 파일을 추가했습니다. 설정
    내용은 다음과 같습니다.
    * End of Line: LF
    * Insert new line at final
    * Indent Style: Space
    * Indent Size: 4
    * Charset: UTF-8
    설정과 맞지 않는 기존의 파일들 또한 설정에 맞도록 변경하였습니다.

--- app/models/documents.go
+++ app/models/documents.go
@@ -6,10 +6,10 @@ import "time"
  * Document - A database model representing Wiki document
  */
 type Document struct {
-	ID			uint
-	Title   string `gorm:"size:255"sql:"not null;"`
-	Content string `sql:"type:text;not null;"`
-	CreatedAt	time.Time
-	UpdatedAt	time.Time
-	DeletedAt	*time.Time
+    ID          uint
+    Title       string `gorm:"size:255"sql:"not null;"`
+    Content     string `sql:"type:text;not null;"`
+    CreatedAt   time.Time
+    UpdatedAt   time.Time
+    DeletedAt   *time.Time
 }
