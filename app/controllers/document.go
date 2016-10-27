5637f25f51a6db8cc85ca0385b405f8f87f30513
Namyun Kim <namyun.me@gmail.com>
2016-10-27 16:18:41 +0900

    위키 문서 작성시 입력값 검증
    사용자가 새로운 위키 문서를 작성할 때 입력값이 올바른지 검사합니다.
    현재는 입력값의 유무만 판정합니다.

--- app/controllers/document.go
+++ app/controllers/document.go
@@ -1,26 +1,46 @@
 package controllers
 
 import (
+    "log"
+
     "github.com/revel/revel"
     "github.com/GDG-SSU/wigo/app"
     "github.com/GDG-SSU/wigo/app/models"
+    "github.com/asaskevich/govalidator"
 )
 
 type Document struct {
     *revel.Controller
 }
 
-func (c Document) Write() revel.Result {
+/**
+ * DocumentForm - A struct representing POST form payload
+ */
+type DocumentForm struct {
+    Title   string  `valid:"required"`
+    Content string  `valid:"required"`
+}
 
+func parseForm(p *revel.Params) (DocumentForm, error) {
+    doc := &DocumentForm{}
+    p.Bind(&(doc.Title), "title")
+    p.Bind(&(doc.Content), "content")
+
+    _, err := govalidator.ValidateStruct(doc)
+    return *doc, err
+}
+
+func (c Document) Write() revel.Result {
     // When the submit button is clicked
     if c.Request.Method == "POST" {
-        var title string
-        var content string
-        c.Params.Bind(&title, "title")
-        c.Params.Bind(&content, "content")
-
-        // Save to database
-        app.DB.Create(&models.Document{Title: title, Content: content})
+        df, err := parseForm(c.Params)
+        if (err == nil) {
+            // Save to database
+            app.DB.Create(&models.Document{Title: df.Title, Content: df.Content})
+        } else {
+            // TEMP: kill the application if there is an form-parsing error
+            log.Fatal(err)
+        }
     }
 
     return c.Render()
