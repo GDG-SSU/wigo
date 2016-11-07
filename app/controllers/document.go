e08ae9aeb21a0dc332b80d446bd0da95961d588c
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-07 13:57:25 +0900

    검색 조건 완전일치 -> 검색어 포함

--- app/controllers/document.go
+++ app/controllers/document.go
@@ -1,47 +1,14 @@
 package controllers
 
 import (
-    "log"
-
     "github.com/revel/revel"
-    "github.com/GDG-SSU/wigo/app"
-    "github.com/GDG-SSU/wigo/app/models"
-    "github.com/asaskevich/govalidator"
+    "fmt"
 )
 
-type Document struct {
-    *revel.Controller
-}
-
-/**
- * DocumentForm - A struct representing POST form payload
- */
-type DocumentForm struct {
-    Title   string  `valid:"required"`
-    Content string  `valid:"required"`
-}
-
-func parseForm(p *revel.Params) (DocumentForm, error) {
-    doc := &DocumentForm{}
-    p.Bind(&(doc.Title), "title")
-    p.Bind(&(doc.Content), "content")
-
-    _, err := govalidator.ValidateStruct(doc)
-    return *doc, err
-}
-
-func (c Document) Write() revel.Result {
-    // When the submit button is clicked
-    if c.Request.Method == "POST" {
-        df, err := parseForm(c.Params)
-        if (err == nil) {
-            // Save to database
-            app.DB.Create(&models.Document{Title: df.Title, Content: df.Content})
-        } else {
-            // TEMP: kill the application if there is an form-parsing error
-            // TODO: Handle the submit or database error
-            log.Fatal(err)
-        }
+func (c App) Document(id int) revel.Result {
+    fmt.Print("document")
+    if c.Request.Method == "GET" {
+        fmt.Print(c.Params)
     }
 
     return c.Render()
--- app/controllers/documents/search.go
+++ app/controllers/documents/search.go
@@ -2,11 +2,11 @@ package controllers
 
 import (
     "github.com/revel/revel"
-    "github.com/asaskevich/govalidator"
-    "github.com/GDG-SSU/wigo/app/models"
-    "github.com/GDG-SSU/wigo/app"
     "log"
+    "github.com/GDG-SSU/wigo/app/models"
     "github.com/GDG-SSU/wigo/app/routes"
+    "github.com/asaskevich/govalidator"
+    "github.com/GDG-SSU/wigo/app"
 )
 
 type SearchDocumentForm struct {
@@ -21,6 +21,7 @@ func parseSearchDocumentForm(p *revel.Params) (SearchDocumentForm, error) {
     return *doc, err
 }
 
+
 func (c Document) Search() revel.Result {
     // When submit button is clicked
     if c.Request.Method == "GET" {
@@ -32,14 +33,16 @@ func (c Document) Search() revel.Result {
 
         var documents []models.Document
 
-        app.DB.Where("Title LIKE ? OR Content LIKE ?", searchDocumentForm.Title, searchDocumentForm.Title).Find(&documents)
+        app.DB.Table("documents").Select("id, title").Where("Title LIKE ? OR Content LIKE ?", "%" + searchDocumentForm.Title + "%", "%" + searchDocumentForm.Title + "%").Find(&documents)
 
         // Check existing document
         if len(documents) == 0 {
             return c.Redirect(routes.Document.Write())
         }
+
         c.RenderArgs["documents"] = documents
         return c.RenderTemplate("Document/search_results.html")
     }
     return c.RenderText("Post is not supported")
 }
+
