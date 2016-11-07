092385e0416cb02e4e136f2eb1b8cd0a96438f00
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-07 16:00:47 +0900

    검색 결과 Pagination

--- app/controllers/documents/search.go
+++ app/controllers/documents/search.go
@@ -7,6 +7,7 @@ import (
     "github.com/GDG-SSU/wigo/app/routes"
     "github.com/asaskevich/govalidator"
     "github.com/GDG-SSU/wigo/app"
+    "fmt"
 )
 
 type SearchDocumentForm struct {
@@ -23,7 +24,7 @@ func parseSearchDocumentForm(p *revel.Params) (SearchDocumentForm, error) {
 
 
 // Document 검색
-func (c Document) Search() revel.Result {
+func (c Document) Search(page int) revel.Result {
     // When submit button is clicked
     if c.Request.Method == "GET" {
         searchDocumentForm, err := parseSearchDocumentForm(c.Params)
@@ -33,14 +34,30 @@ func (c Document) Search() revel.Result {
         }
 
         var documents []models.Document
-        app.DB.Table("documents").Select("id, title").Where("Title LIKE ? OR Content LIKE ?", "%" + searchDocumentForm.Title + "%", "%" + searchDocumentForm.Title + "%").Find(&documents)
+        var pages []int
+        count := 0
+        maxNumOfDocument := 1
+        maxNumOfPage := 5
+
+        app.DB.Limit(maxNumOfDocument).Table("documents").Select("id, title").Where("Title LIKE ? OR Content LIKE ?", "%" + searchDocumentForm.Title + "%", "%" + searchDocumentForm.Title + "%").Count(&count).Offset(maxNumOfDocument * (page - 1)).Find(&documents)
 
         // Check existing document
         if len(documents) == 0 {
             return c.Redirect(routes.Document.Write())
         }
 
+        // Init page list
+        for i := page - (maxNumOfPage >> 1); !(len(pages) >= maxNumOfPage) && i <= count / maxNumOfDocument; i++ {
+            if i < 1 {
+                continue
+            }
+            pages = append(pages, i)
+        }
+
         c.RenderArgs["documents"] = documents
+        c.RenderArgs["searchTitle"] = searchDocumentForm.Title
+        c.RenderArgs["page"] = page
+        c.RenderArgs["pages"] = pages
         return c.RenderTemplate("Document/search_results.html")
     }
     return c.RenderText("Post is not supported")
