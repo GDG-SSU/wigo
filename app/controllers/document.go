29cf14545e6c668a132a9a19f774490aa100a2eb
Namyun Kim <namyun.me@gmail.com>
2016-11-16 21:22:07 +0900

    위키 문서 관련 기능을 하나의 컨트롤러로 통합
    위키 문서와 관련된 기능들은 `Document` 컨트롤러에서 구현됩니다. 분리
    되었던 위키 문서 작성 기능의 구현 내용이 이동되었습니다.

--- app/controllers/document.go
+++ app/controllers/document.go
@@ -1,13 +1,33 @@
 package controllers
 
 import (
+    "log"
+
     "github.com/revel/revel"
     "github.com/GDG-SSU/wigo/app"
     "github.com/GDG-SSU/wigo/app/models"
+    "github.com/asaskevich/govalidator"
 )
 
 type Document struct {
     *revel.Controller
+}
+
+/**
+ * DocumentForm - A struct representing POST form payload
+ */
+type WriteForm struct {
+    Title   string  `valid:"required"`
+    Content string  `valid:"required"`
+}
+
+func parseForm(p *revel.Params) (WriteForm, error) {
+    doc := &WriteForm{}
+    p.Bind(&(doc.Title), "title")
+    p.Bind(&(doc.Content), "content")
+
+    _, err := govalidator.ValidateStruct(doc)
+    return *doc, err
 }
 
 // Document 보기
@@ -17,4 +37,21 @@ func (c Document) View(docId int) revel.Result {
     // TODO: document not found. 404
     c.RenderArgs["document"] = document
     return c.RenderTemplate("Document/document.html")
+}
+
+func (c Document) Write() revel.Result {
+    // When the submit button is clicked
+    if c.Request.Method == "POST" {
+        df, err := parseForm(c.Params)
+        if (err == nil) {
+            // Save to database
+            app.DB.Create(&models.Document{Title: df.Title, Content: df.Content})
+        } else {
+            // TEMP: kill the application if there is an form-parsing error
+            // TODO: Handle the submit or database error
+            log.Fatal(err)
+        }
+    }
+
+    return c.Render()
 }
